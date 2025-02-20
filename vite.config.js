c74d1204cd03a1e08c06c3edc30ed3f0378d4dd2
kond1ter <nikitinmax19@gmail.com>
2025-02-21 01:38:18 +0300

    3

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // base: "/RailBuild",
-  build: {
-    base: "/RailBuild"
-  },
-  dev: {
-    base: "/",
-  }
+  base: "/RailBuild",
 })
